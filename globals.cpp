7149dc9d85b170156560aa9fd99fcbf956c8365c
rekonstar <e80305427@gmail.com>
2025-03-02 15:11:47 +0100

    Delete globals.cpp

--- globals.cpp
+++
@@ -1,6 +0,0 @@
-#include "C:\Users\rootste\source\repos\demon_services\impl\globals.hpp"
-#include <Windows.h>  // Hinzufügen der Windows-Header-Datei
-
-namespace globals {
-    int aimbot_keyz = VK_LCONTROL; // Definition und Initialisierung
-}
