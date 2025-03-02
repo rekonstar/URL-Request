c1b8dff84ff645fb6a9c8b9df80311ee57560142
rekonstar <e80305427@gmail.com>
2025-03-02 15:11:02 +0100

    Delete impl directory

--- impl/render/render.hpp
+++
@@ -1,8 +0,0 @@
-namespace n_render {
-	class c_render {
-	public:
-		auto imgui() -> bool;
-		auto hijack() -> bool;
-		auto render() -> bool;
-	};
-} static n_render::c_render* render = new n_render::c_render();
